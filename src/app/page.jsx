e9647407362a674477e9a96c29e35d2c101e9850
Darkon96 <steponframe2@gmail.com>
2024-02-22 00:00:11 +0300

    creat app

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Battleground from "@/components/battleground";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Battleground />
+    </div>
+  );
+}
---
+++ src/components/battleground.jsx
@@ -0,0 +1,18 @@
+const Battleground = () => {
+    return (
+        <div>
+            <table className="">
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+ 
+export default Battleground;
